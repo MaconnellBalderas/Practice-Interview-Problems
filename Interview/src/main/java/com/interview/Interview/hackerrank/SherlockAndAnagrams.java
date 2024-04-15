24b41cac553440d74622677f3852660f73db4661
MaconnellBalderas <mardaedfam@gmail.com>
2024-04-14 21:18:01 -0500

    Updated Sherlock Anagrams

--- Interview/src/main/java/com/interview/Interview/hackerrank/SherlockAndAnagrams.java
+++ Interview/src/main/java/com/interview/Interview/hackerrank/SherlockAndAnagrams.java
@@ -12,13 +12,18 @@ public class SherlockAndAnagrams {
     public static int sherlockAndAnagrams(String s) {
         // Write your code here
         int counter = 0;
-
-        for(int x = 1; x <= s.length() - 1; x++) {
+        // abba
+        for(int x = 1; x <= s.length() - x; x++) {
             for(int i = 0; i < s.length() - x; i++) {
                 for(int j = i + x; j < s.length() - x; j++) {
+                    // Issue Here
+                    System.out.println("I: " + i);
+                    System.out.println("First: " + s.substring(i, i+x));
+                    System.out.println("J: " + j);
+                    System.out.println("Seconds: " + s.substring(j, j+x));
 
-
-                    if(s.substring(i, i+x).equals(s.substring(j, j+x))) {
+                    if(isAnagram(s.substring(i, i+x), s.substring(j, j+x))) {
+                        System.out.println("----------Counter-----------------------");
                         counter++;
                     }
 
@@ -29,15 +34,37 @@ public class SherlockAndAnagrams {
         return counter;
     }
 
-    private boolean isAnagram(String str1, String str2) {
+    private static boolean isAnagram(String str1, String str2) {
         // mma -> mam
-        HashMap map = new HashMap();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-        for(int i = 0; i < str1.length(); i++) {
-//            Character subStr = 
-//            map.put(0, 0);
+        for(int i = 0; i < str1.length() - 1; i++) {
+            String subStr = str1.substring(i, i + 1);
+
+            if(map.containsKey(subStr)) {
+               int updateCount = map.get(subStr) + 1;
+
+               map.put(subStr, updateCount);
+            } else {
+                map.put(subStr, 1);
+            }
         }
 
-        return false;
+        for(int i = 0; i < str2.length() - 1; i++) {
+            String subStr = str2.substring(i, i + 1);
+
+            if(map.containsKey(subStr)) {
+                if(map.get(subStr) == 0) {
+                    return false;
+                } else {
+                    int updatedCount = map.get(subStr) - 1;
+                    map.put(subStr, updatedCount);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return map.isEmpty();
     }
 }
